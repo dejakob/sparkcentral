246f5a50c1d15a6e70c3cc7c58441be4aba9fda7
dejakob <jakobvti@gmail.com>
2016-01-21 19:29:22 +0100

    Add unit tests color animation

--- spec/animation/color-animation.spec.js
+++ spec/animation/color-animation.spec.js
@@ -32,6 +32,33 @@ describe('ColorAnimation class ', () => {
             expect(ColorAnimation.prototype.onTick).toBeDefined();
         });
 
+        it('should call animation.onTick', () => {
+            spyOn(Animation.prototype, 'onTick').and.callFake(() => {});
+            colorAnimation.onTick(0.5);
+            expect(Animation.prototype.onTick).toHaveBeenCalled();
+        });
 
+        it('should set the current value to a color value based on percentage', () => {
+            colorAnimation.onTick(0.5);
+            expect(colorAnimation.currentValue).toBe('rgb(45,45,92)');
+            colorAnimation.onTick(0.2);
+            expect(colorAnimation.currentValue).toBe('rgb(18,36,73)');
+            colorAnimation.onTick(0.75);
+            expect(colorAnimation.currentValue).toBe('rgb(67,52,108)');
+            colorAnimation.onTick(0);
+            expect(colorAnimation.currentValue).toBe('rgb(0,30,60)');
+            colorAnimation.onTick(1);
+            expect(colorAnimation.currentValue).toBe('rgb(90,60,125)');
+
+            colorAnimation.from = RgbColor.fromArray([ 0, 0, 0 ]);
+            colorAnimation.to = RgbColor.fromArray([ 255, 0, 0 ]);
+
+            colorAnimation.onTick(0);
+            expect(colorAnimation.currentValue).toBe('rgb(0,0,0)');
+            colorAnimation.onTick(0.5);
+            expect(colorAnimation.currentValue).toBe('rgb(127,0,0)');
+            colorAnimation.onTick(1);
+            expect(colorAnimation.currentValue).toBe('rgb(255,0,0)');
+        });
     });
 });
